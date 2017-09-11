4cdba7b6750d0104ac5ca623cfee25e598d88553
Casto Colina <castocolina@gmail.com>
2017-09-11 14:53:24 -0300

    fix arguments values & default filemode

--- config-gitrepo.sh
+++ config-gitrepo.sh
@@ -15,30 +15,35 @@ if [ $# -eq 0 ] || [ -z "$1" ]  || [ -z "$2" ]; then
     #echo -n "Enter your email > "
     #read useremail
     read -p "Enter your email (default: '$useremail')> " iuseremail
-
-    [ ! -z "$iusername" ] && username=$iusername
-    [ ! -z "$iuseremail" ] && useremail=$iuseremail
 else
     iusername="$1"
     iuseremail="$2"
 fi
 
+[ ! -z "$iusername" ] && username=$iusername
+[ ! -z "$iuseremail" ] && useremail=$iuseremail
+
 git config --local --unset-all user.name
 git config --local --unset-all user.email
 git config --local --replace-all user.name "$username"
 git config --local --replace-all user.email "$useremail"
-git config --local core.filemode true
+#git config --local core.filemode false
+#git config --local --unset-all core.filemode
 git config --global core.editor nano
 git config --global push.default simple
 #git config --global --unset core.editor
 git config --global core.longpaths true
 
+## https://help.github.com/articles/dealing-with-line-endings/
 if [ "$(uname)" == "Darwin" ]; then
     git config --global core.autocrlf input
+    echo "Mac ::: git config --global core.autocrlf input"
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     git config --global core.autocrlf input
+    echo "Linux ::: git config --global core.autocrlf input"
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
     git config --global core.autocrlf true
+    echo "WIN ::: git config --global core.autocrlf true"
 fi
 
 git config -l
