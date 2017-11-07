9cb6fcb37efca7a751bdbc620e841a10b7c0afbe
Casto Colina <castocolina@gmail.com>
2017-11-06 22:19:21 -0300

    update scripts

--- install-jdk6.sh
+++ install-jdk6.sh
@@ -11,11 +11,20 @@ sudo mkdir -p $INSTALL_JAVA6_HOME
 sudo mv jdk1.6*/* $INSTALL_JAVA6_HOME
 rm -rf jdk1.6*/
 
+JAVA6="JAVA6_HOME=$INSTALL_JAVA6_HOME"
+if ! grep -q "$JAVA6" ~/.profile; then
+    echo "" >> ~/.profile
+    echo "export $JAVA6" >> ~/.profile
+    echo "JAVA6 SET"
+    source $HOME/.profile
+fi
 
-sudo update-alternatives --install "/usr/bin/java" "java" "$INSTALL_JAVA6_HOME/bin/java" 2
-sudo update-alternatives --install "/usr/bin/javac" "javac" "$INSTALL_JAVA6_HOME/bin/javac" 2
-#sudo update-alternatives --install "/usr/bin/javaws" "javaws" "$INSTALL_JAVA7_HOME/bin/javaws" 1
+$JAVA6_HOME/bin/java -version
+
+sudo update-alternatives --install "/usr/bin/java" "java" "$INSTALL_JAVA6_HOME/bin/java" 1080
+sudo update-alternatives --install "/usr/bin/javac" "javac" "$INSTALL_JAVA6_HOME/bin/javac" 1080
+sudo update-alternatives --install "/usr/bin/javaws" "javaws" "$INSTALL_JAVA6_HOME/bin/javaws" 1080
 
 sudo update-alternatives --config java
 sudo update-alternatives --config javac
-#sudo update-alternatives --config javaw
+sudo update-alternatives --config javaws
--- install-jdk7.sh
+++ install-jdk7.sh
@@ -11,11 +11,20 @@ sudo mkdir -p $INSTALL_JAVA7_HOME
 sudo mv jdk1.7*/* $INSTALL_JAVA7_HOME
 rm -rf jdk1.7*/
 
+JAVA7="JAVA7_HOME=$INSTALL_JAVA7_HOME"
+if ! grep -q "$JAVA7" ~/.profile; then
+    echo "" >> ~/.profile
+    echo "export $JAVA7" >> ~/.profile
+    echo "JAVA7 SET"
+    source $HOME/.profile
+fi
 
-sudo update-alternatives --install "/usr/bin/java" "java" "$INSTALL_JAVA7_HOME/bin/java" 1
-sudo update-alternatives --install "/usr/bin/javac" "javac" "$INSTALL_JAVA7_HOME/bin/javac" 1
-#sudo update-alternatives --install "/usr/bin/javaws" "javaws" "$INSTALL_JAVA7_HOME/bin/javaws" 1
+$JAVA7_HOME/bin/java -version
+
+sudo update-alternatives --install "/usr/bin/java" "java" "$INSTALL_JAVA7_HOME/bin/java" 1080
+sudo update-alternatives --install "/usr/bin/javac" "javac" "$INSTALL_JAVA7_HOME/bin/javac" 1080
+sudo update-alternatives --install "/usr/bin/javaws" "javaws" "$INSTALL_JAVA7_HOME/bin/javaws" 1080
 
 sudo update-alternatives --config java
 sudo update-alternatives --config javac
-#sudo update-alternatives --config javaw
+sudo update-alternatives --config javaws
