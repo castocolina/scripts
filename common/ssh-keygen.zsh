fa586399ac65902163352fb35a72c72cc2d944f7
Casto Colina <castocolina@gmail.com>
2019-08-20 19:33:25 -0700

    protect ssh key file

--- common/ssh-keygen.zsh
+++ common/ssh-keygen.zsh
@@ -21,6 +21,8 @@ if [ ! -f "$SSH_KEYFILE" ] ; then
     echo "CREATE SSH KEY FOR $SSH_KEYFILE"
 
     ssh-keygen -t rsa -f $SSH_KEYFILE -b 4096 -C $SSH_KEYNAME
+        chmod 600 $SSH_KEYNAME
+    chmod 600 $SSH_KEYNAME.pub
 fi
 
 eval `ssh-agent`
