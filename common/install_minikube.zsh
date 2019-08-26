4aebd1a8f90985aaadb0dd1d38eeb5f96eec316e
Casto Colina <castocolina@gmail.com>
2019-08-26 11:47:21 -0400

    install hyperkit in osx

--- common/install_minikube.zsh
+++ common/install_minikube.zsh
@@ -52,6 +52,17 @@ if [ "$MY_OS" = "linux" ]; then
 
 fi
 
+if [ "$MY_OS" = "darwin" ]; then
+  (is_false $to_update && exist_cmd docker-machine-driver-hyperkit) || {
+    echo "INSTALL HYPERKIT";
+    curl -LO https://storage.googleapis.com/minikube/releases/latest/docker-machine-driver-hyperkit \
+    && chmod +x docker-machine-driver-hyperkit \
+    && sudo mv docker-machine-driver-hyperkit /usr/local/bin/ \
+    && sudo chown root:wheel /usr/local/bin/docker-machine-driver-hyperkit \
+    && sudo chmod u+s /usr/local/bin/docker-machine-driver-hyperkit
+  }
+fi
+
 (is_false $to_update && exist_cmd kubectl) || {
   echo "INSTALL KUBECTL";
   if [ "$MY_OS" = "linux" ]; then
